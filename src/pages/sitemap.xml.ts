15be9d74478546e111ae4b31e9b2e2ad6a13a3e5
kimsinh <53991529+saroto@users.noreply.github.com>
2025-12-22 09:04:01 +0700

    update site map and robots.txt

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,14 @@
+export async function GET() {
+  const siteUrl = import.meta.env.SITE;
+
+  const result = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>${siteUrl}</loc></url>
+</urlset>`;
+
+  return new Response(result, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
