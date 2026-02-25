83df4097928d8705b05591df6601f3cd64a190b0
kimsinh <53991529+saroto@users.noreply.github.com>
2026-02-25 15:10:32 +0700

    feat: Integrate shadcn/ui components, refactor portfolio sections for improved styling, and add a dark mode toggle.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://kimsinh.vercel.app/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 });
